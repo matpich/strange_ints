69e6d6c56ea65de7be2737fbc462bd4b4b7e88b0
Mateusz Pichniarczyk <mat.pich@interia.pl>
2018-06-18 14:15:37 +0200

    Overload some operators.

--- strange_ints.py
+++ strange_ints.py
@@ -1,8 +1,55 @@
 
+class Sint():
+    def __init__(self, int_num):
+        self.int_num = int_num
 
+    def __add__(self, other):
+        return Sint(int(str(self.int_num) + str(other.int_num)))
+
+    def __sub__(self, other):
+        temp_value = list(str(self.int_num))
+        temp_value.reverse()
+        [temp_value.remove(i) for i in list(str(other.int_num)) if i in temp_value]
+        temp_value.reverse()
+        temp_value = ''.join(temp_value)
+        return Sint(int(temp_value))
+
+    def __mul__(self, other):
+        temporary = [int(i) for i in list(str(other.int_num))]
+        multiplier = int(sum(temporary)/len(temporary))
+        return Sint(int(str(self.int_num)*multiplier))
+
+    def __truediv__(self, other):
+        temporary = [int(i) for i in list(str(other.int_num))]
+        divider = str(int(sum(temporary)/len(temporary)))
+        return Sint(int(str(self.int_num).replace(divider,'0')))
+
+
+    def __str__(self):
+        return str(self.int_num)
+
+
+    def __int__(self):
+        return self.int_num
 
 def main():
- pass
+    x = Sint(123)
+    print(x)
+    y = Sint(456)
+    print(y)
+
+    e = x+y
+    print(e)
+
+    e = x-y
+    print(e)
+
+    e = x*y
+    print(e)
+
+    e /= e
+    print(e)
+
 
 if __name__ == '__main__':
     main()
